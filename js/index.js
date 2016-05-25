51bd8cfcf46bed3cc57031faf21e43b8b7fd3f4a
Jeff <killerzebra69@gmail.com>
2016-05-25 08:17:29 -0400

    facebook

--- js/index.js
+++ js/index.js
@@ -54,6 +54,16 @@ $(document).ready(function()
          }
 
       });
+
+   (function(d, s, id)
+   {
+     var js, fjs = d.getElementsByTagName(s)[0];
+     if (d.getElementById(id)) return;
+     js = d.createElement(s); js.id = id;
+     js.src = "//connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.6&appId=133777333699751";
+     fjs.parentNode.insertBefore(js, fjs);
+   }(document, 'script', 'facebook-jssdk'));
+
 });
 
 function pagination(pages)
